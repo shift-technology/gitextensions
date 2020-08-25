07d137b2a934471de2dfa98fae3134cc5e6cd936
Justin Marshall <justin.marshall@shift-technology.com>
2020-08-25 14:34:43 +0200

    Installation

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
 @echo off
-powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0scripts\build.ps1""" -restore -build -bl %*"
+powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0scripts\build.ps1""" -restore -build -publish -c Release -bl %*"
 exit /b %ErrorLevel%
